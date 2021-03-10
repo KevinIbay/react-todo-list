9d2feca181d2f5bc9e91dcd5ee55c18df05718c5
Kevin Ibay <kevinibaycs@gmail.com>
2021-03-10 13:09:08 -0600

    revert to localhost from heroku

--- src/App.js
+++ src/App.js
@@ -20,9 +20,7 @@ function App() {
 
   // Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch(
-      'https://evening-caverns-14371.herokuapp.com/tasks'
-    );
+    const res = await fetch('http://localhost:5000/tasks');
     const data = await res.json();
 
     return data;
@@ -30,9 +28,7 @@ function App() {
 
   // Fetch Single Task
   const fetchTask = async (id) => {
-    const res = await fetch(
-      `https://evening-caverns-14371.herokuapp.com/tasks/${id}`
-    );
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
     const data = await res.json();
 
     return data;
@@ -40,16 +36,13 @@ function App() {
 
   // Add Task
   const addTask = async (task) => {
-    const res = await fetch(
-      'https://evening-caverns-14371.herokuapp.com/tasks',
-      {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(task),
-      }
-    );
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    });
 
     const data = await res.json();
 
@@ -61,7 +54,7 @@ function App() {
 
   // Delete Task
   const deleteTask = async (id) => {
-    await fetch(`https://evening-caverns-14371.herokuapp.com/tasks/${id}`, {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'DELETE',
     });
     setTasks(tasks.filter((task) => task.id !== id));
@@ -72,16 +65,13 @@ function App() {
     const taskToToggle = await fetchTask(id);
     const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(
-      `https://evening-caverns-14371.herokuapp.com/tasks/${id}`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(updatedTask),
-      }
-    );
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updatedTask),
+    });
 
     const data = await res.json();
 
